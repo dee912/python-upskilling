a9ac8b6fcab80c049bbeecc2c91a7622d7698e9b
Devante <devantespaine@gmail.com>
2022-07-07 16:35:14 +0100

    lists

---
+++ intro-to-python/lists.py
@@ -0,0 +1,15 @@
+my_list = ['eggs', 'cheese', 'ham'];
+
+# print(my_list[0]);
+# print(my_list[- 1]);
+
+my_appended_list = []
+
+for item in my_list:
+    my_appended_list.append(item);
+
+print(my_appended_list);
+
+my_reversed_list = my_list[::-1];
+
+print(my_reversed_list);
