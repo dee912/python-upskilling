a9b411f5439ddb3e38fd063d65b47f190d4022cd
Devante <devantespaine@gmail.com>
2022-07-07 13:57:50 +0100

    setting json

---
+++ intro-to-python/strings.py
@@ -0,0 +1,20 @@
+basic_string = 'this is my basic string';
+print(basic_string);
+
+escaped_string = '\"Hello there\", he said';
+print(escaped_string);
+
+multiline_string = '''
+    This is a multiline string
+    
+    It will respect whitespace
+'''
+print(multiline_string);
+
+name = 'Devante';
+
+age = 26;
+
+concat_string = 'Hello my name is ' + name + ', I am ' + str(age) + ' years old';
+
+print(concat_string);
