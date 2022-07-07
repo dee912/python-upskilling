8fb447ad69b1e12facf50b967ca63860e15dd58e
Devante <devantespaine@gmail.com>
2022-07-07 14:45:34 +0100

    booleans

---
+++ intro-to-python/booleans.py
@@ -0,0 +1,13 @@
+print(bool(False));
+print(bool(0));
+print(bool(''));
+print(bool(None));
+print(bool([]));
+print(bool({}));
+
+print(bool(True));
+print(bool(1));
+print(bool(' '));
+print(bool(['hello', 'hi']));
+print(bool(2.3));
+print(bool({'name': 'devante'}));
