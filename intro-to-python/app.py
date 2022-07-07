d75834d192fedb4953d4c9e23bc95d689740a1d5
Devante <devantespaine@gmail.com>
2022-07-07 14:59:49 +0100

    conditionals

--- intro-to-python/app.py
+++ intro-to-python/app.py
@@ -1,6 +1,13 @@
-this_thing = False;
+name = input('What is your name? ');
 
-if this_thing:
-    print('the thing is true');
-else: 
-    print('the thing is false');
+if name == 'Devante':
+    print('Hi Devante');
+elif name == 'Emma' or name == 'Hugo':
+    print('Hi Emma or Hugo');
+else:
+    print('Hi Stranger');
+
+thing = False;
+
+if not thing:
+    print('Only runs when thing is falsey');
